c30da78dbdcc53708a2ec6cd886ee07294ae74c8
Ronald Iván <roniaq@gmail.com>
2022-09-29 17:42:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int a;
+Console.Write("Ingrese numero de escalones de la piramide: ");
+a = int.Parse(Console.ReadLine());
+for (int y = 1; y <= a; y++)
+{
+    for (int x = 1; x <= a - y; x++)
+    {
+        Console.Write(" ");
+    }
+    for (int x = 1; x <= y; x++)
+    {
+        Console.Write("*");
+    }
+
+    for (int x = 1; x <= y - 1; x++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+
+}
